c44e38aca6376ce08957325f3451fdee3352eb20
Romain BERNARD <30secondstodraw@gmail.com>
2016-11-09 12:19:12 +0100

    Add files via upload

--- Board.java
+++ Board.java
@@ -3,15 +3,19 @@ package asd.proj.model;
 import java.awt.Color;
 import java.util.Random;
 import asd.proj.Constante;
+import java.util.ArrayList;
 
 class Board{
 
 	private Case[][] _grid;
+	private Case[] _starsp1;
+	private Case[] _starsp2;
 
 	public Board(){
 		Random rand= new Random();
 		int randx= nextInt(length);
 		int randy= nextInt(length);
+		_grid = new Case[length][length];
 
 		for(Case tile : _grid)
 			tile = new Case();
@@ -33,6 +37,32 @@ class Board{
 			}
 		}
 		
+	}
+
+	public Case[][] getGrid{
+		return _grid;
+	}
+
+	public void setCase(int x, int y, Color color, ClassUnion union){
+		ArrayList<ClassUnion> list = this.relieComposante(x,y,color);
+		if (list.size()==0){
+			_grid[x][y].setColor(color);
+		} else {
+			for(ClassUnion class : list ){
+				class.union();
+			}
+		}
+	}
+
+	public ArrayList<ClassUnion> relieComposante(int x, int y, Color color){
+
+		ArrayList<ClassUnion> possibility = new ArrayList<ClassUnion>();
+		for (int i=x-1; i<x+2; ++i){
+			for(int j=y-1;j<y+2; ++i){
+				if(_grid[i][j].getColor()==color)
+					possibility.add(_grid[i][j].getClass());
+			}
+		}
 	}
 
 }
